72b945a93a6e788b513e954a4162c52317408e7e
jdaintry <47262749+jdaintry@users.noreply.github.com>
2019-02-04 20:42:55 +1100

    Completed the module

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -8,7 +8,7 @@ import {Album} from '../album';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  albumInfo ; Album;
+  albumInfo: Album;
 
   constructor(private _productService: ProductService) { }
 
